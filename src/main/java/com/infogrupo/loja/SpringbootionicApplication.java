d6b026212f9dc7688415bc6fac3a4f5189be97fc
Carlos Lopes <Carlos Lopes@DESKTOP-NQQH900>
2019-08-10 16:44:14 -0300

    Primeira versão - Criação do projeto

---
+++ src/main/java/com/infogrupo/loja/SpringbootionicApplication.java
@@ -0,0 +1,13 @@
+package com.infogrupo.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootionicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootionicApplication.class, args);
+	}
+
+}
