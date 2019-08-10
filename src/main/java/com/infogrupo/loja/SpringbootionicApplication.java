b8299018d045b001f7ad3cfe66eb22cf3aae2f97
Carlos Lopes <Carlos Lopes@DESKTOP-NQQH900>
2019-08-10 17:56:56 -0300

    Criando operação de instanciação

--- src/main/java/com/infogrupo/loja/SpringbootionicApplication.java
+++ src/main/java/com/infogrupo/loja/SpringbootionicApplication.java
@@ -1,13 +1,33 @@
 package com.infogrupo.loja;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringbootionicApplication {
+import com.infogrupo.loja.entity.Categoria;
+import com.infogrupo.loja.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringbootionicApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootionicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
